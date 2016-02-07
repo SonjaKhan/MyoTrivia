b3bc43a9d180894d9dd27c2d242032da29debd53
Sonja Khan <sonjak3@uw.edu>
2016-02-06 20:20:28 -0800

    added a status component

--- src/client/component/AppComponent.jsx
+++ src/client/component/AppComponent.jsx
@@ -1,13 +1,40 @@
 var React = require('react');
+var Myo = require('myo');
+
 var Constants = require('../Constants');
 var QuestionComponent = require('./QuestionComponent.jsx');
 var BrandingComponent = require('./BrandingComponent.jsx');
 var MenuComponent = require('./MenuComponent.jsx');
 var CategoryPicker = require('./CategoryPicker.jsx');
+var MyoStatusComponent = require('./MyoStatusComponent.jsx');
 
 var AppComponent = React.createClass({
     getInitialState : function() {
-        return {};
+        return {
+            myo: null,
+        };
+    },
+
+    componentWillMount : function() {
+        var self = this;
+        
+        Myo.onError = function() {
+            console.log("Myo Connect is not running");
+        };
+        
+        Myo.on('connected', function() {
+            console.log('connected', this);
+            self.setState({
+                myo: this,
+            });
+            self.forceUpdate();
+        });
+
+        Myo.on('disconnected', function() {
+            self.forceUpdate();
+        });
+
+        Myo.connect("com.sonjakhan.myoTrivia");
     },
 
     render : function() {
@@ -30,6 +57,7 @@ var AppComponent = React.createClass({
 
         return (
             <div id="app">
+                <MyoStatusComponent myo={this.state.myo} />
                 <BrandingComponent />
                 { content }
             </div>
